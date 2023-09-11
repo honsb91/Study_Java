9af5991aa58266de71de0b1405bbf505b7ae0eb2
honsb91 <114128759+honsb91@users.noreply.github.com>
2023-09-11 17:42:08 +0900

    class 생성 후이니셜 콘솔 출력
    class 생성 및 출력

---
+++ WorkSpace/01.HelloWorld/src/HelloWorld.java
@@ -0,0 +1,15 @@
+// 기계가 컴파일 하지 않는 코드 . 개발자가 보기 위한 설명 .
+// main : 메인 메소드라고 하면 우리가 당분가 계속 해서 사용할 프로그램의 시작점 .
+// syso : 프로그램이 진행 되는 중에 어떤 문자나 결과를 출력하기 위한 명령어 .
+// ctrl + space : 코드의 자동 완성 .
+// ctrl + s 또는 ctrl + shfit + s : 현재 파일 저장 , 전체 파일 저장 .
+
+
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("HelloWorldNSB!");
+	}
+
+}
