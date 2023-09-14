e4414610755ea7dcc057bc164539cc0d1e5ddc76
honsb91 <114128759+honsb91@users.noreply.github.com>
2023-09-14 17:13:30 +0900

    타입 변환 공부
    타입 변환 공부

---
+++ WorkSpace/02.Variable/src/pack02/variabletype/Test_VariableType.java
@@ -0,0 +1,36 @@
+package pack02.variabletype;
+
+public class Test_VariableType {
+	public static void main(String[] args) {
+		
+		// 알고있는 모든 변수타입 초기화하거나 선언 후 출력해보기
+		
+		int ok;
+		ok = 1;
+		System.out.println(ok);
+		
+		short short_v = 1;
+		System.out.println(short_v);
+		
+		byte byte_v = 3;
+		System.out.println(byte_v);
+		
+		char char_v = 'A';
+		System.out.println(char_v);
+		
+		long long_v = 120L;
+		System.out.println(long_v);
+		
+		boolean t = true;
+		boolean f = false;
+		System.out.println(t);
+		System.out.println(f);
+		
+		
+	}
+	
+	{
+		short ok_v = 1;
+		System.out.println(ok_v);
+	}
+}
