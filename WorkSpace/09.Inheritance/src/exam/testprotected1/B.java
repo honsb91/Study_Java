4a1d5c5fdb08f9f5cfd2d81cbb5c179591cac523
honsb91 <114128759+honsb91@users.noreply.github.com>
2023-10-18 15:34:40 +0900

    상속관계
    상속

---
+++ WorkSpace/09.Inheritance/src/exam/testprotected1/B.java
@@ -0,0 +1,20 @@
+package exam.testprotected1;
+
+public class B{
+	public B() {
+		
+	}
+	
+	public void initA() {
+		A a = new A();// protected 가 같은 패키지 내부에서 사용이 가능함.
+	}
+	
+	public A rtnA() {
+		return new A();
+	}
+	
+	public int rtnAfield() {
+		A a = new A();
+		return a.protected_field4; // = 1;
+	}
+}
